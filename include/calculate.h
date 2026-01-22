07a446393acc94941727522cb5d26c7cb6eedfd1
idz <akshayksajivan@gmail.com>
2026-01-22 21:08:33 +0530

    Revert "Reapply "updated with documentation in readme file""
    This reverts commit 1c554ba0d85978d6b0adb3def22f876410e2d3f6.

--- include/calculate.h
+++
@@ -1,6 +0,0 @@
-#ifndef CALCULATE_H
-#define CALCULATE_H
-
-#include "student.h"
-void calculatetotal(student *data);
-#endif
--- include/validate.h
+++
@@ -1,6 +0,0 @@
-#ifndef VALIDATE_H
-#define VALIDATE_H
-
-#include "student.h"
-int validate(student data);
-#endif
